17b803129378ce127c891229939eb7d29abf9317
will Farrell <will.farrell@gmail.com>
2017-02-05 09:22:29 -0700

    update nginx bootstrap
    - faster runtime - load self-signed cert if no cert then wait to update nginx when reat cert is generated
    - no need for NGINX_ENV now

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,29 +2,35 @@
 set -e
 #set -x
 
+update_hpkp() {
+    if [ ! -f /etc/ssl/${NGINX_DOMAIN}/cert.pem ]; then
+        echo "docker run willfarrell/letsencrypt ..."
+        sleep 5
+        update_hpkp &
+    else
+        echo "non-selfcertificate found"
+        /etc/scripts/make_hpkp
+        nginx -t
+        exec "${@}"
+    fi
+
+}
 if [ "${1}" = 'nginx' ]; then
 
     # replace all set ${ENV}
     /etc/scripts/nginx_env
     /etc/scripts/make_csp
 
-    if [ "${NGINX_ENV}" == 'staging' ] || [ "${NGINX_ENV}" == 'production' ]; then
-        while [ ! -f /etc/ssl/${NGINX_DOMAIN}/privkey.pem ] \
-            || [ ! -f /etc/ssl/${NGINX_DOMAIN}/cert.pem ] \
-            || [ ! -f /etc/ssl/${NGINX_DOMAIN}/chain.pem ] \
-            || [ ! -f /etc/ssl/${NGINX_DOMAIN}/fullchain.pem ]; do
-            echo "docker run willfarrell/letsencrypt ..."
-            sleep 10;
-         done;
-
-         /etc/scripts/make_hpkp
-    else
+    if [ ! -f /etc/ssl/${NGINX_DOMAIN}/cert.pem ]; then
         /etc/scripts/make_ecdsa_cert
+        rm /etc/ssl/${NGINX_DOMAIN}/cert.pem
+        update_hpkp &
+    else
+        # calls: nginx -t && exec "${@}"
+        update_hpkp
     fi
 
-    # validate configs
     nginx -t
-
 fi
 
 echo "${@}"
