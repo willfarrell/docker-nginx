02c0a7ed0e07525cbaa1a4968ae7ac62854a6a15
will Farrell <willfarrell@users.noreply.github.com>
2018-07-13 14:46:42 -0600

    fix: undo

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,13 +5,12 @@ set -e
 if [ "${1}" = 'nginx' ]; then
 
     # replace all set ${ENV}
-    /etc/scripts/nginx_env /etc/nginx
+    /etc/scripts/nginx_env
     /etc/scripts/make_csp
 
     /etc/scripts/bootstrap_https
     
     # custom startup script
-    /etc/scripts/nginx_env /var/www
     /etc/scripts/bootstrap
 
 fi
